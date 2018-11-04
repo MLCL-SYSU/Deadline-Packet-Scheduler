e0ace7b0b698af001f66cc5db57a050fdbc1fc29
marc <marcmolla@gmail.com>
2018-11-04 22:03:23 +0100

    Implemented allowed congestion

--- scheduler.go
+++ scheduler.go
@@ -294,6 +294,9 @@ func (sch *scheduler) selectPathDQNAgent(s *session, hasRetransmission bool, has
 	for pathID, pth := range s.paths{
 		cong := pth.sentPacketHandler.GetCongestionWindow()-pth.sentPacketHandler.GetBytesInFlight()
 		allowed := pth.SendingAllowed() || (cong <= 0 && float32(cong) >=  -float32(pth.sentPacketHandler.GetCongestionWindow()) * float32(sch.AllowedCongestion) * 0.01)
+		if !pth.SendingAllowed(){
+			utils.Errorf("Not allowed: %f, %f", cong, -float32(pth.sentPacketHandler.GetCongestionWindow()) * float32(sch.AllowedCongestion) * 0.01)
+		}
 		if pathID != protocol.InitialPathID && (allowed || hasRetransmission){
 			availablePaths = append(availablePaths, pathID)
 			sRTT[pathID] = pth.rttStats.SmoothedRTT()
